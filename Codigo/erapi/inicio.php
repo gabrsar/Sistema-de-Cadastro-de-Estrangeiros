e0d62c909cf7639f75403cb0cc84d48dc705a311
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-15 09:22:40 -0300

    Nova página de inicio

--- Codigo/erapi/inicio.php
+++ Codigo/erapi/inicio.php
@@ -1,1 +1,7 @@
-INCIO =)
+<div id="menu_principal">
+	<ul>
+		<li> <a href="index.php?page=novoCadastro">Novo cadastro</a></li>
+		<li> <a href="index.php?page=configuracoes">Configurações</a></li>
+		<li> <a href="index.php?page=relatorios">Relatórios</a></li>
+	</ul>
+</div>
