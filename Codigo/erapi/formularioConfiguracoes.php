adfd3f7e0219293c34c58d6af804748b884c0527
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-15 09:19:10 -0300

    Página de configurações

---
+++ Codigo/erapi/formularioConfiguracoes.php
@@ -0,0 +1,7 @@
+<div id="menu">
+	<ul>
+		<li> <a href="index.php?page=configuracoesDepartamentos">Departamentos</a></li>
+		<li> <a href="index.php?page=configuracoesCursos">Cursos</a> </li>
+		<li> <a href="index.php?page=configuracoesUsuarios">Usuários</a> </li>
+	</ul>
+</div>
