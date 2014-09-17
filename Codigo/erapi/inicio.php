bea3e153f0736d84ec0aa5626a496999ccd9c37b
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-16 22:27:41 -0300

    Adequação do inicio.php com o padrão atual

--- Codigo/erapi/inicio.php
+++ Codigo/erapi/inicio.php
@@ -1,7 +1,12 @@
-<div id="menu_principal">
+<div id="menu_principal" class="painelHorizontal">
 	<ul>
-		<li> <a href="index.php?page=novoCadastro">Novo cadastro</a></li>
-		<li> <a href="index.php?page=configuracoes">Configurações</a></li>
+		<li> <a href="index.php?page=novoCadastro">Estrangeiros</a></li>
 		<li> <a href="index.php?page=relatorios">Relatórios</a></li>
+		<li> <a href="index.php?page=configuracoes">Configurações</a></li>
 	</ul>
+</div>
+<div id="pendentes">
+	<p> Cadastros aguardando validação </p>
+	<table>
+	</table>
 </div>
