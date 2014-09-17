b69ed89b8d6724814b04b4475f2126495afeaec7
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-16 22:26:56 -0300

    Correção e adaptação na página Configurações

--- Codigo/erapi/formularioConfiguracoes.php
+++ Codigo/erapi/formularioConfiguracoes.php
@@ -1,4 +1,8 @@
-<div id="menu">
+<div id="titulo">
+	<a href="index.php?page=inicio" class="voltar">&lt&lt</a>
+	<p class="titulo">Configurações</p>
+</div>
+<div id="menu" class="painel">
 	<ul>
 		<li> <a href="index.php?page=configuracoesDepartamentos">Departamentos</a></li>
 		<li> <a href="index.php?page=configuracoesCursos">Cursos</a> </li>
