3ac14e773dff5803f9f7b27f522f7af0e4f73740
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-24 10:12:03 -0300

    Altera login.php, para uma página isolada

--- Codigo/erapi/login.php
+++ Codigo/erapi/login.php
@@ -1,24 +1,39 @@
 <?php
-
 	/* Autor: Gabriel Henrique Martinez Saraiva
- 	 * Pagina em que o usuário faz login.
- 	 *
- 	 * #FIXME - Preciso corrigir o sistema de redirecionamento para que essa 
- 	 * página possa fazer login.
+	 * Pagina em que o usuário faz login.
+	 *
+	 * Essa página é a única que fica fora do index.php,
+	 * isso acontece para evitar que seja necessário carregar o banco de dados
+	 * aqui dentro. E também para simplificar o script de redirecionamento e 
+	 * login, não tendo que tratar esse caso que o usuário ainda não está logado
+	 * .
  	 */
-
-echo $_SESSION['passDB'];
-echo "<br>";
-echo $_SESSION['passFORM'];
 ?>
 
-<div id="login">
-<h4> EXECUTE O SCRIPT fakeUsers.php PARA CRIAR OS USUÁRIOS </h4>
-	<form action="index.php?page=scriptLogin" method="post">
-		<p><label>Usuário</label><input type="text" name="login"></p>
-		<p><label>Senha</label><input type="password" name="senha"></p>
-		<div class="botoesLogin">
-			<button>Entrar</button>
+<!DOCTYPE html>
+<html lang="pt_BR">
+	<head>
+		<?php
+			include ("simplex/head.php");
+		?>
+		<script src="js/jquery-latest.js"></script>
+	</head>
+	<body>
+   	<div id="pagina">
+			<?php
+				include ("simplex/topo.php");
+			?>
+			<div id="corpo">
+				<div id="login">
+					<form action="index.php?page=scriptLogin" method="post">
+						<p><label>Usuário</label><input type="text" name="login"></p>
+						<p><label>Senha</label><input type="password" name="senha"></p>
+						<div class="botoesLogin">
+							<button>Entrar</button>
+						</div>
+					</form>
+				</div>
+	    	</div>
 		</div>
-	</form>
-</div>
+	</body>
+</html>
