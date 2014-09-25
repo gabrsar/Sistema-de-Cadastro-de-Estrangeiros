8eb508d321d6cb0ca75c1ee946bce20c6b5ddb24
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-25 20:57:57 -0300

    Adiciona exemplo na tela de inicio

--- Codigo/erapi/inicio.php
+++ Codigo/erapi/inicio.php
@@ -13,8 +13,88 @@
 		<li> <a href="index.php?page=scriptSair">Sair</a></li>
 	</ul>
 </div>
-<div id="pendentes">
-	<p> Cadastros aguardando validação </p>
+<div class="listagem">
+	<p class="titulo"> Lista de cadastros pendentes </p>
 	<table>
+		<thead>
+			<tr><td>Data</td><td>Nome</td><td>Curso</td><td>Professor</td><td>Pais</td></tr>
+		</thead>
+		<tbody>
+			<?php
+
+			$departamentos = R::find('departamento','excluido=0');
+			foreach ($departamentos as $dep) {
+
+				$id=$dep->id;
+				$nome=$dep->nome;
+				$link="confirmarRemoverDepartamento(\"$nome\",$id)";
+
+				echo ("<tr>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+			}
+			?>
+		</tbody>
+		<tfoot>
+			<tr><td colspan="5">Foram encontrados 
+				<?php echo (R::count('departamento','excluido=0')); ?>
+			registros</td></tr>
+		</tfoot>
+	</table>
+</div>
+
+<div class="listagem">
+	<p class="titulo"> Lista dos últimos cadastros confirmados </p>
+	<table>
+		<thead>
+			<tr><td>Data</td><td>Nome</td><td>Curso</td><td>Professor</td><td>Pais</td></tr>
+		</thead>
+		<tbody>
+			<?php
+
+			$departamentos = R::find('departamento','excluido=0');
+			foreach ($departamentos as $dep) {
+
+				$id=$dep->id;
+				$nome=$dep->nome;
+				$link="confirmarRemoverDepartamento(\"$nome\",$id)";
+
+				echo ("<tr>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+				echo("<td>");
+				echo("<p>...</p>");
+				echo("</td>");
+			}
+			?>
+		</tbody>
+		<tfoot>
+			<tr><td colspan="5">Foram encontrados 
+				<?php echo (R::count('departamento','excluido=0')); ?>
+			registros</td></tr>
+		</tfoot>
 	</table>
 </div>
