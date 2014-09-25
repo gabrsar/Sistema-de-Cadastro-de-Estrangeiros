03bb776533a38e6d32b00c6cf180dfbaf6bf59b4
Gabriel Saraiva <extremez3r0@gmail.com>
2014-09-25 20:58:39 -0300

    Arquivo com lista de permissoes

---
+++ Codigo/erapi/permissao.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Autor: Gabriel Henrique Martinez Saraiva
+ * Esse arquivo armazena principalmente permissões dos usuários.
+ *
+ */
+
+$PERMISSOES = array(
+	"ADMINISTRADOR" => 0,
+	"USUARIO" => 1,
+	"ESPECTADOR" => 2
+);
+
+
+
+?>
