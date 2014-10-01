63b21c1e7a94b15a90d8a4c165324e956619ad9e
Gabriel Saraiva <extremez3r0@gmail.com>
2014-10-01 10:29:10 -0300

    Implementa função para obter o ID da permissão do usuaário

--- Codigo/erapi/permissao.php
+++ Codigo/erapi/permissao.php
@@ -19,6 +19,26 @@ class Permissao{
 		return self::$tipos;
 	}
 
+	static public function getIDPermissao($texto)
+	{
+
+		// Essa função não é escalavel. Mas como o número de usuários do
+		// sistema é pequeno não há problemas. Caso o sistema possua mais de
+		// 100 usuários ou essa função seja utilizada muitas vezes, 
+		// ela pode começar a ser um gargalo no sistema.
+		// Um dia eu penso em algo mais "melhor".
+
+		foreach(self::$tipos as $nome => $tipo)
+		{
+			if($tipo[1] == $texto)
+			{
+				return $tipo[0];
+			}
+		}
+		return "ID INESISTENTE!";
+
+	}
+
 	static public function getNomePermissao($id)
 	{
 		if($id < sizeof(self::$tipos))
