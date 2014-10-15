4111904ff056c45bcc4763a1fb32c1ee5616c874
Victor Hugo <victorhcandido@gmail.com>
2014-10-15 14:46:13 -0300

    Adiciona formularioEstrangeiros, que lista todos os estrangeiros

---
+++ Codigo/erapi/formularioEstrangeiros.php
@@ -0,0 +1,21 @@
+<?php
+
+	/* Autor: Victor Hugo Cândido de Oliveira
+ 	 * Página que exibe todos os estrangeiros cadastrados.
+ 	 * Permite realização de um novo cadastro de estrangeiro.
+ 	 */
+
+	require("subformularioTabela.php");
+?>
+
+
+<div id="titulo">
+	<a href="index.php?page=inicio" class="voltar">&lt&lt</a>
+	<p class="titulo">Estrangeiros</p>
+</div>
+<div class="listagem">
+	<p class="titulo">Lista de estrangeiros cadastrados</p>
+		<?php
+			mostrarTabelaEstrangeiros();	
+		?>
+</div>
